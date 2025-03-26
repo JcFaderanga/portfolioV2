ed5e2dec683c828f06b1d948eef517a340d61ecd
Jeysi/ <119491303+JcFaderanga@users.noreply.github.com>
2025-03-26 22:49:04 +0800

    Modify: text size and add details

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -22,43 +22,46 @@ export default function Home() {
         <div className={`hidden lg:flex w-full h-[20%] absolute top-0  shadow-lg ${style.slide} bg-white`}></div>
           <div className={`${visible && style.popIn}`}>
             <h1 
-              className={`${visible ? `block text-3xl font-bold text-center text-shadow` : `hidden`}`}
+              className={`${visible ? `block text-3xl md:text-5xl font-semibold text-center text-shadow ` : `hidden`}`}
             >
-              {`${personalInfo.first_name} ${personalInfo.last_name}`}
+              <span className="text-lg font-normal">I am</span> {`${personalInfo.first_name} ${personalInfo.last_name}`}
             </h1>
             <p 
-              className={`${visible ? `text-lg text-center text-shadow `: `hidden`}`}
+              className={`${visible ? `text-lg text-center text-shadow py-1 leading-10`: `hidden`}`}
             >
-            I am a <span className="font-bold">{position}</span> From Philippines
+             <span className="text-xl font-normal">a</span> <span className="font-semibold text-3xl">
+              Software Tester <span className="text-xl font-normal">and</span>  Front End Developer 
+            </span>
             </p>
+            <h2 className={`${visible ? `text-4xl font-normal text-center text-shadow` : `hidden`}`}> <span className="text-xl font-normal">From</span>  Philippines</h2>
             <div 
               className={`${visible ? `block text-lg cursor-pointer font-bold text-center py-5` : `hidden`} `}
             >
-            <div className="mb-6">
+            <div className="mt-6 mb-5">
                 <ul 
                   className="flex item-center justify-center px-2 
                   after:bg-[#333333] after:mt-3 after:w-16 after:border after:h-0.5 after:ml-5
                   before:bg-[#333333] before:mt-3 before:w-16 before:border before:h-0.5 before:mr-5
                   ">
-                    <li className="px-1">
+                    <li className="px-1 hover:scale-125" title="facebook">
                       <a href={socialMedia.facebook}> 
                         <Facebook size={25} color="#333333" />
                       </a>
                     </li>
-                    <li className="px-1">
+                    <li className="px-1 hover:scale-125" title="linkedin">
                     <a href={socialMedia.linkedin}>
                         <Linkedin size={25} color="#333333" />
                       </a>
                     </li>
-                    <li className="px-1">
-                    <a href={socialMedia.github}>
+                    <li className="px-1 hover:scale-125">
+                    <a href={socialMedia.github} title="github">
                         <Github size={25} color="#333333" />
                       </a>
                     </li>
                 </ul>
             </div>
             <Link href="/home">
-              <span className=" hover:bg-gray-100 border-gray-300 rounded-md py-1 px-4 mt-4">KNOW MORE</span>
+              <span className="bg-gray-100 hover:bg-gray-200 border-gray-300 rounded-md py-1 px-4 mt-4">KNOW MORE</span>
             </Link>
             </div>
           </div>
